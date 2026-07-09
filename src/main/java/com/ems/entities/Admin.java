f20bdae2b224e22f5b6f748dcbba270cf19991f9
harshitham1974 <harshitham1974@gmail.com>
2026-07-09 19:28:03 +0530

    Configure JPA utility

--- src/main/java/com/ems/entities/Admin.java
+++ src/main/java/com/ems/entities/Admin.java
@@ -1,5 +1,44 @@
 package com.ems.entities;
 
-public class Admin {
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
 
+public class Admin {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO,generator = "admin_seq")
+	@SequenceGenerator(name = "admin_seq",initialValue = 1,allocationSize = 1)
+	private int id;
+    private String username;
+    private String password;
+	public Admin(int id, String username, String password) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Admin [id=" + id + ", username=" + username + "]";
+	}
+    
 }
