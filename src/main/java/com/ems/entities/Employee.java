1ed4e3691c4442329802522bf106c64197fe3c6c
harshitham1974 <harshitham1974@gmail.com>
2026-07-09 19:07:21 +0530

    Create Employee entity

---
+++ src/main/java/com/ems/entities/Employee.java
@@ -0,0 +1,79 @@
+package com.ems.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+
+@Entity
+public class Employee {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO,generator = "emp_seq")
+	@SequenceGenerator(name = "emp_seq",initialValue = 1,allocationSize = 1)
+	private int id;
+    private String name;
+    private String email;
+    private String phone;
+    private String department;
+    private double salary;
+    private LocalDate joiningDate;
+    private String password;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getDepartment() {
+		return department;
+	}
+	public void setDepartment(String department) {
+		this.department = department;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	public LocalDate getJoiningDate() {
+		return joiningDate;
+	}
+	public void setJoiningDate(LocalDate joiningDate) {
+		this.joiningDate = joiningDate;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", email=" + email + ", phone=" + phone + ", department="
+				+ department + ", salary=" + salary + ", joiningDate=" + joiningDate + ", password=" + password + "]";
+	}
+    
+    
+}
