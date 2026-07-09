465bffc4fbf5453162e4138faba1cca5270ab8ba
harshitham1974 <harshitham1974@gmail.com>
2026-07-09 19:44:13 +0530

    Create Employee DAO

---
+++ src/main/java/com/ems/dao/EmployeeDAO.java
@@ -0,0 +1,15 @@
+package com.ems.dao;
+
+import com.ems.entities.Employee;
+
+public class EmployeeDAO {
+
+	public void save(Employee employee) {
+		
+	}
+
+	public void findByEmail(String email) {
+		
+	}
+
+}
