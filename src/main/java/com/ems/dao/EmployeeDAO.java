8a4c562d42fd1bb77067367b65c852c3c622754f
harshitham1974 <harshitham1974@gmail.com>
2026-07-10 19:59:07 +0530

    Implement EmployeeDAO

---
+++ src/main/java/com/ems/dao/EmployeeDAO.java
@@ -0,0 +1,75 @@
+package com.ems.dao;
+
+import com.ems.entities.Employee;
+import com.ems.util.JPAUtil;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.NoResultException;
+import jakarta.persistence.TypedQuery;
+
+public class EmployeeDAO {
+
+	public void save(Employee employee) {
+		EntityManager entityManager = JPAUtil.getEntityManager();
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        entityTransaction.begin();
+
+        entityManager.persist(employee);
+
+        entityTransaction.commit();
+
+        entityManager.close();
+	}
+
+	 public Employee findByEmail(String email) {
+
+	        EntityManager entityManager = JPAUtil.getEntityManager();
+
+	        String jpql = "SELECT e FROM Employee e WHERE e.email = :email";
+
+	        TypedQuery<Employee> query =
+	                entityManager.createQuery(jpql, Employee.class);
+
+	        query.setParameter("email", email);
+
+	        try {
+	            return query.getSingleResult();
+	        } catch (NoResultException e) {
+	            return null;
+	        } finally {
+	            entityManager.close();
+	        }
+	    }
+	 public boolean existsByEmail(String email) {
+
+		    EntityManager em = JPAUtil.getEntityManager();
+
+		    String jpql = "SELECT e FROM Employee e WHERE e.email=:email";
+
+		    TypedQuery<Employee> query =
+		            em.createQuery(jpql, Employee.class);
+
+		    query.setParameter("email", email);
+
+		    try {
+
+		        query.getSingleResult();
+
+		        return true;
+
+		    } catch (NoResultException e) {
+
+		        return false;
+
+		    } finally {
+
+		        em.close();
+
+		    }
+
+		}
+
+}
